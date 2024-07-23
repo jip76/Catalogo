de7fa3aa82f80ee0cb4fb60ab02bafa33aa0040c
jorgeplaza <jip76@hotmail.com>
2024-07-23 18:01:28 -0500

    Patron de repositorio

---
+++ Catalogo.Acceso.Datos/Repositorio/IRepositorio/IBodegaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Catalogo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IBodegaRepositorio:IRepositorio<Bodega>
+    {
+        void Actualizar(Bodega bodega);
+    }
+}
---
+++ Catalogo.Acceso.Datos/Repositorio/Repositorio.cs
@@ -0,0 +1,90 @@
+﻿using Catalogo.AccesoDatos.Data;
+using Catalogo.AccesoDatos.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task Agregar(T entidad)
+        {
+            await dbSet.AddAsync(entidad); // insert into table
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if(incluirPropiedades != null)
+            {
+                foreach (var incluirPro in incluirPropiedades.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirPro);
+                }
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
+        }
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirPro in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirPro);
+                }
+            }
+            
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }        
+
+        public void Remover(T entidad)
+        {
+            dbSet.Remove(entidad);
+        }
+
+        public void removerRango(IEnumerable<T> entidad)
+        {
+            dbSet.RemoveRange(entidad);
+        }
+    }
+}
