9418dab8ce101db2ae3a850fb668f3ac9d52b6b3
jorgeplaza <jip76@hotmail.com>
2024-07-26 17:04:51 -0500

    CRUD de Marca

---
+++ Catalogo.Modelos/Marca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.Modelos
+{
+    public class Marca
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo nombre es requerido")]
+        [MaxLength(60,ErrorMessage ="Nombre maximo de 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripcion es requerido")]
+        [MaxLength(150, ErrorMessage = "Descripcion maximo de 150 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
